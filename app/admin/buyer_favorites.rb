ae4814003182d7290e1f038c2ea6620bfa05bbdd
anonymous anonymous <brittany.borg@kellogg.northwestern.edu>
2021-12-05 02:53:29 +0000

    Whitelist columns in BuyerFavorite for ActiveAdmin

--- app/admin/buyer_favorites.rb
+++ app/admin/buyer_favorites.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BuyerFavorite do
 
+ permit_params :user_id, :puppy_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
